fe3a283bc75884d51e62fc0c1195917c0be96008
Taofik Araba <arabataofik@gmail.com>
2022-03-09 22:59:05 +0200

    Assignment is partially done

--- src/test/java/Pages/AddSummerDressWithGPage.java
+++ src/test/java/Pages/AddSummerDressWithGPage.java
@@ -2,20 +2,21 @@ package Pages;
 
 import Base.TestBase;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.Select;
 
 public class AddSummerDressWithGPage extends TestBase {
-    public int random;
 
-    @FindBy(how = How.CLASS_NAME, using = "replace-2x img-responsive")
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"center_column\"]/ul/li[4]/div/div[2]/h5/a")
     public static WebElement PrintedSDWGlasses;
 
-    @FindBy(how = How.XPATH, using = "//*[@id=\"homefeatured\"]/li[6]/div/div[2]/div[2]/a[2]/span")
-    public static WebElement ClickOnMore;
+    //@FindBy(how = How.XPATH, using = "//*[@id=\"homefeatured\"]/li[6]/div/div[2]/div[2]/a[2]/span")
+    //public static WebElement ClickOnMore;
 
-    @FindBy(how = How.XPATH, using = "//*[@id=\"quantity_wanted_p\"]/a[2]/span/i")
+    @FindBy(how = How.XPATH, using = "//*[@id=\"quantity_wanted_p\"]/a[2]/span")
     public static WebElement IncreaseQuantityByOne;
 
 
@@ -51,7 +52,7 @@ public class AddSummerDressWithGPage extends TestBase {
     @FindBy(how = How.XPATH, using = "//*[@id=\"form\"]/p/button/span")
     public static WebElement ProceedAgain;
 
-    @FindBy(how = How.ID_OR_NAME, using = "Pay by bank wire")
+    @FindBy(how = How.XPATH, using = "//*[@id=\"HOOK_PAYMENT\"]/div[1]/div/p/a")
     public static WebElement ClickPaymentMethod;
 
     @FindBy(how = How.XPATH, using = "//*[@id=\"cart_navigation\"]/button/span")
@@ -60,13 +61,49 @@ public class AddSummerDressWithGPage extends TestBase {
     @FindBy(how = How.XPATH, using = "//*[@id=\"center_column\"]/div/p/strong")
     public static WebElement CompleteOrderMessage;
 
+    @FindBy(how = How.XPATH, using = "//*[@id=\"contact-link\"]/a")
+    public static WebElement ContactUsIcon;
 
-    public void HoverOnThePSDWGlasses() {PrintedSDWGlasses.getLocation();}
-    public void More() {ClickOnMore.click();}
+    @FindBy(how = How.ID, using = "id_contact")
+    public static WebElement SubjectHeadings;
+
+    @FindBy(how = How.ID_OR_NAME, using = "id_order")
+    public static WebElement OrderReference;
+
+    @FindBy(how = How.ID, using = "408253_order_products")
+    public static WebElement SelectProduct;
+
+    @FindBy(how = How.ID, using = "fileUpload")
+    public static WebElement ChooseFiles;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"block_top_menu\"]/ul/li[2]/a")
+    public static WebElement DRESSES;
+
+    //@FindBy(how = How.XPATH, using = "//*[@id=\"fileUpload\"]")
+    //public static WebElement ChooseFile;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"message\"]")
+    public static WebElement MessageAboutMyProduct;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"submitMessage\"]/span")
+    public static WebElement ClickTheSendKey;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"center_column\"]/p")
+    public static WebElement OrderSuccessMessage;
+
+
+
+
+
+    public void TopDresses() {DRESSES.click();}
+    public void HoverOnThePSDWGlasses() {PrintedSDWGlasses.click();
+    }
+
+    //public void More() {ClickOnMore.click();}
     public void IncreaseProductQty() {IncreaseQuantityByOne.click();}
     public void SelectSize() {
         Select ClothSize = new Select (DressSize);
-        ClothSize.selectByValue("L");
+        ClothSize.selectByIndex(2);
     }
     public void AddToCartButton() {ClickAddToCartButton.click();}
     //wait five seconds
@@ -82,5 +119,21 @@ public class AddSummerDressWithGPage extends TestBase {
     public void ProceedTOCheOut() {ProceedAgain.click();}
     public void SelectPaymentMethod() {ClickPaymentMethod.click();}
     public void ConfirmOrder() {ClickConfirmOrder.click();}
-    public void ConfirmationMessage() {CompleteOrderMessage.isDisplayed();}
+    public void Confirmation() {CompleteOrderMessage.click();}
+    public void ClickContactUsLogo() {ContactUsIcon.click();}
+    public void SelectSubjectHeading() {
+        Select SubjectHeading = new Select (SubjectHeadings);
+        SubjectHeading.selectByIndex(1);}
+    public void SelectOrderReference() {
+        Select OrderReferences = new Select (OrderReference);
+        OrderReferences.selectByIndex(2);}
+    //public void SelectTheProduct() {
+       // Select SelectTheProducts = new Select (SelectProduct);
+        //SelectTheProducts.selectByIndex(1);}
+
+    //public void ClickOnChooseFiles() {ChooseFile.click();}
+
+    public void MessageToSupport() {MessageAboutMyProduct.sendKeys("I want it fitted");}
+    public void ClickOnSend() {ClickTheSendKey.click();}
+    public void ConfirmationMessage() {OrderSuccessMessage.isDisplayed();}
 }
--- src/test/resources/features/AddSummerDressWithG.feature
+++ src/test/resources/features/AddSummerDressWithG.feature
@@ -3,27 +3,36 @@ Feature: Add to cart
   Background:
     Given I am on the application homepage
     And I select the sign link
-    When I enter valid username
-    And I enter valid password
-    When I click the login button
-    Then I should be logged in successfully
+    When I enter valid usernames
+    And I enter valid passwords
+    When I click login button
+    Then I should see the application homepages
 
 
   Scenario: Add two printed summer dress with glasses to cart.
 
+    Given I click on dresses
     Given I hover on product
-    And I click on more
+    #And I click on more
     And I change product quantity
     And I change product size
     And I add product to cart
     And I confirm my order is successful
     When I proceed to checkout
     And I confirm the total sum is correct
-    When I click on proceed to checkout
+    When I click on proceed to checkouts
     And I enter message to the support team
     And I proceed to checkout again
     And I agree the terms
-    When I click on proceed to checkout
+    #When I click on proceed to checkout
     And I select payment method
     When I confirm my order
     Then My order should be send successfully
+    When I click on contact uss
+    And I select customer services
+    And I selected my order reference
+    #And i select product
+    #When I click on choose file
+    And I write message to support
+    When I click on send
+    Then My message should be sent
